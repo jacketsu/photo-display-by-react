aa5b8a1fdb52b7a67b6cd6489a5074541feed0ae
WEISU <jacketsu@hotmail.com>
2017-08-04 00:36:45 -0400

    init structure

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,30 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.scss');
+
+var imageDatas = require('../data/imageDatas.json');
+
+imageDatas = (function genImageURL(imageDataArr) {
+	for (var i = 0, j = imageDataArr.length; i < j; i++) {
+		var singleImageData = imageDataArr[i];
+
+		singleImageData.imageURL = require('../images/' + singleImageData.fileName);
+
+		imageDataArr[i] = singleImageData;
+	}
+	return imageDataArr;
+})(imageDatas);
 
 import React from 'react';
-
-let yeomanImage = require('../images/yeoman.png');
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+    	<section className="stage">
+    		<section className="img-sec">
+    		</section>
+    		<nav className="controller-nav">
+    		</nav>
+    	</section>
     );
   }
 }
