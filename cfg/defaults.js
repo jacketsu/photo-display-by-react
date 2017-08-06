86f979d022f59c76d28c94009759749127226e5f
WEISU <jacketsu@hotmail.com>
2017-08-06 09:53:22 -0400

    furnish

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -62,7 +62,7 @@ function getDefaultModules() {
 
 module.exports = {
   srcPath: srcPath,
-  publicPath: '/assets/',
+  publicPath: 'photo-display-by-react/assets/',
   port: dfltPort,
   getDefaultModules: getDefaultModules
 };
